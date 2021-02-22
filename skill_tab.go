dcad8102c73a3e512f61ba45c9713196e9dd6dc5
Nic Johnson <nicjohnson145@hotmail.com>
2021-02-22 12:02:22 -0600

    Convert skills/saves to cards as well

--- skill_tab.go
+++ skill_tab.go
@@ -20,71 +20,49 @@ func (s *sheet) skillTab() fyne.CanvasObject {
 }
 
 func (s *sheet) skills() fyne.CanvasObject {
-	profs := []fyne.CanvasObject{}
-	names := []fyne.CanvasObject{}
-	mods := []fyne.CanvasObject{}
+	contents := []fyne.CanvasObject{}
 	for _, skill := range s.character.AllSkills {
-		profs = append(profs, profWidget(func() bool { return s.character.isProficientInSkill(skill) }))
-		names = append(names, widget.NewLabel(skill.Name))
-		mods = append(mods, widget.NewLabel(s.character.modStringForSkill(skill)))
+		contents = append(
+			contents,
+			fyne.NewContainerWithLayout(
+				layout.NewHBoxLayout(),
+				profWidget(func() bool { return s.character.isProficientInSkill(skill) }),
+				widget.NewLabel(skill.Name),
+				widget.NewLabel(s.character.modStringForSkill(skill)),
+			),
+		)
 	}
 
-	return fyne.NewContainerWithLayout(
-		layout.NewVBoxLayout(),
+	return widget.NewCard(
+		"Skills",
+		"",
 		fyne.NewContainerWithLayout(
-			layout.NewCenterLayout(),
-			widget.NewLabel("Skills"),
-		),
-		widget.NewSeparator(),
-		fyne.NewContainerWithLayout(
-			layout.NewHBoxLayout(),
-			fyne.NewContainerWithLayout(
-				layout.NewGridLayout(1),
-				profs...,
-			),
-			fyne.NewContainerWithLayout(
-				layout.NewGridLayout(1),
-				names...,
-			),
-			fyne.NewContainerWithLayout(
-				layout.NewGridLayout(1),
-				mods...,
-			),
+			layout.NewVBoxLayout(),
+			contents...,
 		),
 	)
 }
 
 func (s *sheet) saves() fyne.CanvasObject {
-	profs := []fyne.CanvasObject{}
-	names := []fyne.CanvasObject{}
-	mods := []fyne.CanvasObject{}
+	contents := []fyne.CanvasObject{}
 	for _, save := range s.character.AllSavingThrows {
-		profs = append(profs, profWidget(func() bool { return s.character.isProficientInSave(save) }))
-		names = append(names, widget.NewLabel(save))
-		mods = append(mods, widget.NewLabel(s.character.modStringForSave(save)))
+		contents = append(
+			contents,
+			fyne.NewContainerWithLayout(
+				layout.NewHBoxLayout(),
+				profWidget(func() bool { return s.character.isProficientInSave(save) }),
+				widget.NewLabel(save),
+				widget.NewLabel(s.character.modStringForSave(save)),
+			),
+		)
 	}
 
-	return fyne.NewContainerWithLayout(
-		layout.NewVBoxLayout(),
+	return widget.NewCard(
+		"Saves",
+		"",
 		fyne.NewContainerWithLayout(
-			layout.NewCenterLayout(),
-			widget.NewLabel("Saves"),
-		),
-		widget.NewSeparator(),
-		fyne.NewContainerWithLayout(
-			layout.NewHBoxLayout(),
-			fyne.NewContainerWithLayout(
-				layout.NewGridLayout(1),
-				profs...,
-			),
-			fyne.NewContainerWithLayout(
-				layout.NewGridLayout(1),
-				names...,
-			),
-			fyne.NewContainerWithLayout(
-				layout.NewGridLayout(1),
-				mods...,
-			),
+			layout.NewVBoxLayout(),
+			contents...,
 		),
 	)
 }
