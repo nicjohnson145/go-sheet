e39eee78d551d0d91505255dbe328e5fb77eb32d
Nic Johnson <nicjohnson145@hotmail.com>
2021-05-31 20:16:32 -0500

    feat: add interactivity for resource entries

--- resources.go
+++ resources.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
+	"fyne.io/fyne/v2/dialog"
 )
 
 func (s *sheet) resourceTab() fyne.CanvasObject {
@@ -16,7 +18,7 @@ func (s *sheet) resourceTab() fyne.CanvasObject {
 		if idx > 0 {
 			resources = append(resources, widget.NewSeparator())
 		}
-		resources = append(resources, s.resourceRow(*resource))
+		resources = append(resources, s.resourceRow(*resource, idx))
 	}
 
 	return container.NewVScroll(
@@ -27,10 +29,37 @@ func (s *sheet) resourceTab() fyne.CanvasObject {
 	)
 }
 
-func (s *sheet) resourceRow(resource Resource) fyne.CanvasObject {
+func (s *sheet) resourceRow(resource Resource, idx int) fyne.CanvasObject {
 	return fyne.NewContainerWithLayout(
 		layout.NewHBoxLayout(),
-		widget.NewLabel(resource.Name),
+		widget.NewButton(
+			resource.Name,
+			func() {
+				e := widget.NewEntry()
+				dialog.ShowCustomConfirm(
+					fmt.Sprintf("Adjust %v", resource.Name),
+					"Add",
+					"Remove",
+					e,
+					func(b bool) {
+						val, err := strconv.Atoi(e.Text)
+						if err != nil {
+							fmt.Println(err)
+							return
+						}
+						if b {
+							s.character.Resources[idx].Current += val
+							fmt.Println(fmt.Sprintf("Adjusting %v up %v", resource.Name, val))
+						} else {
+							s.character.Resources[idx].Current -= val
+							fmt.Println(fmt.Sprintf("Adjusting %v down %v", resource.Name, val))
+						}
+						s.writeReadCharacter()
+					},
+					s.window,
+				)
+			},
+		),
 		layout.NewSpacer(),
 		widget.NewLabel(fmt.Sprintf("%v / %v", resource.Current, resource.Max)),
 	)
